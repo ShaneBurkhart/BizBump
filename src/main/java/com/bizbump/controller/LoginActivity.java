fb0d15c5c2c85d3ae753f269906bf14517138e52
ShaneBurkhart <shaneburkhart@gmail.com>
2013-08-13 22:34:13 -0500

    Very basic Oauth

---
+++ src/main/java/com/bizbump/controller/LoginActivity.java
@@ -0,0 +1,51 @@
+package com.bizbump.controller;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.accounts.AccountManagerCallback;
+import android.accounts.AccountManagerFuture;
+import android.os.Bundle;
+import android.accounts.OperationCanceledException;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+
+import android.accounts.AuthenticatorException;
+
+import java.io.IOException;
+
+/**
+ * Created by Shane on 8/13/13.
+ */
+public class LoginActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        AccountManager am = AccountManager.get(this);
+        Account[] accounts = am.getAccountsByType("com.google");
+
+        am.getAuthToken(accounts[0], "Manage your tasks", new Bundle(), this, new OnTokenAcquired(), new Handler());
+    }
+
+    private class OnTokenAcquired implements AccountManagerCallback<Bundle> {
+        @Override
+        public void run(AccountManagerFuture<Bundle> result) {
+            try{
+                // Get the result of the operation from the AccountManagerFuture.
+                Bundle bundle = result.getResult();
+                // The token is a named value in the bundle. The name of the value
+                // is stored in the constant AccountManager.KEY_AUTHTOKEN.
+                String token = bundle.getString(AccountManager.KEY_AUTHTOKEN);
+                Log.d("Account - Token", token);
+            } catch (IOException e){
+                e.printStackTrace();
+            } catch (OperationCanceledException e){
+                e.printStackTrace();
+            } catch (AuthenticatorException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
